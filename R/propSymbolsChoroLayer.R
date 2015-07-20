94649af9370c019b62dce2467e8aa27cb73b236a
rCarto <timothee.giraud@ums-riate.fr>
2015-07-20 16:57:28 +0200

    propSymbolsChoroLayer legends  + code

--- R/propSymbolsChoroLayer.R
+++ R/propSymbolsChoroLayer.R
@@ -1,86 +1,79 @@
-#' @title Proportional Symbols Typo Layer
+#' @title Proportional and Choropleth Layer
 #' @name propSymbolsChoroLayer
-#' @description Plot a proportional symbols layer with a color based on a qualitative data. Various symbols are availables.
-#' @param spdf Spatial*DataFrame; if \code{spdf} is a SpatialPolygonsDataFrame 
-#' symbols are plotted on centroids.
-#' @param df data.frame; \code{df} contains the values to plot.
-#' @param spdfid character; id field in \code{spdf}, default to the first column 
-#' of the \code{spdf} data.frame. (optional)
-#' @param dfid character; id field in \code{df}, default to the first column 
-#' of \code{df}. (optional)
-#' @param var character; name of the numeric field in \code{df} to plot.
-#' @param var2 character; name of the factor field in \code{df} to plot.
-#' @param distr numeric; a vector of breaks. (see Details)
-#' @param nclass numeric; a targeted number of classes (if null,the Huntsberger 
-#' method is used).
+#' @description Plot a proportional symbols layer with color based on a quantitative data discrtization. 
+#' @param spdf SpatialPointsDataFrame or SpatialPolygonsDataFrame; if spdf 
+#' is a SpatialPolygonsDataFrame symbols are plotted on centroids.
+#' @param df data.frame; df contains the values to plot.
+#' @param spdfid character; id field in spdf, default to the first column 
+#' of the spdf data.frame. (optional)
+#' @param dfid character; id field in df, default to the first column 
+#' of df. (optional)
+#' @param var character; name of the numeric field in df to plot the symbols sizes.
+#' @param var2 character; name of the numeric field in df to plot the colors.
+#' @param breaks numeric; break points in sorted order to indicate the intervals for assigning the colors. 
+#' Note that if there are nlevel colors (classes) there should be (nlevel+1) breakpoints (see Details).
+#' @param col character; a vector of colors. Note that if breaks is specified there must be one less 
+#' colors specified than the number of break. 
+#' @param nclass numeric; a targeted number of classes. If null, the number of class is automatically defined (see Details).
 #' @param method character; a discretization method; one of "sd", "equal", 
-#' "quantile", "jenks","q6","geom"  (see Details)
-#' @param symbols character; type of symbols, one of "circles", "squares" or "height").
-#' @param col character; a vector of colors (hues).
-#' @param k numeric; share of the map occupied by the biggest symbol.
-#' @param fixmax numeric; value of the biggest symbol. (optional)
-#' @param legend.var.pos character; position of the legend var, one of "topleft", "top", 
-#' @param legend.var2.pos character; position of the legend var2, one of "topleft", "top", 
-#' "topright", "left", "right", "bottomleft", "bottom", "bottomright".
-#' @param legend.title.var.txt character; title of the legend (numeric data).
-#' @param legend.title.var2.txt character; title of the legend (factor data).
+#' "quantile", "jenks","q6","geom"  (see Details).
+#' @param symbols character; type of symbols, one of "circle", "square" or "bar".
+#' @param k numeric; share of the map occupied by the biggest symbol (see Details).
+#' @param fixmax numeric; value of the biggest symbol (see Details).
+#' @param border character; color of polygon borders.
+#' @param lwd numeric; borders width.
+#' @param legend.var.pos character; position of the legend, one of "topleft", "top", 
+#' "topright", "left", "right", "bottomleft", "bottom", "bottomright". If 
+#' legend.var.pos is "n" then the legend is not plotted.
+#' @param legend.var2.pos character; position of the legend, one of "topleft", "top", 
+#' "topright", "left", "right", "bottomleft", "bottom", "bottomright". If 
+#' legend.var2.pos is "n" then the legend is not plotted.
+#' @param legend.var.title.txt character; title of the legend (numeric data).
+#' @param legend.var2.title.txt character; title of the legend (factor data).
 #' @param legend.title.cex numeric; size of the legend title.
 #' @param legend.values.cex numeric; size of the values in the legend.
-#' @param legend.values.rnd numeric; number of decimal places of the values in 
+#' @param legend.var.values.rnd numeric; number of decimal places of the values in 
 #' the legend.
-#' @param legend.style character; either "a" or "b". The legend has two display 
+#' @param legend.var2.values.rnd numeric; number of decimal places of the values in 
+#' the legend.
+#' @param legend.var.style character; either "a" or "b". The legend has two display 
 #' styles.
-#' @param legend.frame boolean; whether to add a frame to the legend (TRUE) or 
+#' @param legend.var.frame boolean; whether to add a frame to the legend (TRUE) or 
 #' not (FALSE).
+#' @param legend.var2.frame boolean; whether to add a frame to the legend (TRUE) or 
+#' not (FALSE).
+#' @param legend.var2.nodata  text for legend.nodata
 #' @param add boolean; whether to add the layer to an existing plot (TRUE) or 
 #' not (FALSE).
-#' @return A plot is returned.
-#' @export
-#' @import sp
 #' @examples
 #' data("nuts2006")
-#' # Layout plot
-#'layoutLayer(title = "Countries Population in Europe", 
-#'            sources = "UMS RIATE, 2015", 
-#'            author = "UMS RIATE",
-#'            scale = NULL, 
-#'            frame = TRUE,
-#'            col = "black", 
-#'           coltitle = "white",
-#'            bg = "#D9F5FF",
-#'            south = TRUE, 
-#'            extent = nuts0.spdf)
-#' #Countries plot
-#' plot(nuts0.spdf, col = "grey60",border = "grey20", add=TRUE)
-#' #Population plot on proportional symbols
-#'#colours <- palette(rainbow(length(nuts0.spdf)))
-#'#propSymbolsTypoLayer(spdf = nuts0.spdf, df = nuts0.df,
-#'#                     var = "pop2008", var2="id", k = 0.01,
-#'#                     symbols = "circles", col =  colours,
-#'#                     legend.var.pos = "topleft", legend.var2.pos = "topright",
-#'#                     legend.title.var.txt = "Total\npopulation (2008)",
-#'#                     legend.title.var2.txt = "Types de trucs",
-#'#                     legend.style = "b")
+#' ## Exemple 1
+#' # Countries plot
+#' plot(nuts0.spdf, col = "grey60",border = "grey20", add=FALSE)
+#' # Population and growth rate 
+#' nuts0.df$cagr <- (((nuts0.df$pop2008 / nuts0.df$pop1999)^(1/9)) - 1) * 100
+#' propSymbolsChoroLayer(spdf = nuts0.spdf, df = nuts0.df,var = "pop2008", var2 = "cagr", 
+#'                       legend.var.values.rnd = -3, legend.var2.values.rnd = 2)
+#' @export
+#' @import sp
 propSymbolsChoroLayer <- function(spdf, df, spdfid = NULL, dfid = NULL,
-                                 var,
-                                 var2,
-                                 symbols = "circles",
-                                 col = NULL,
-                                 nclass=NULL,
-                                 distr=NULL,
-                                 method="quantile",
-                                 k = 0.02, fixmax = NULL,
-                                 legend.var.pos = "bottomleft",legend.var2.pos = "bottomleft", legend.title.var.txt = var,
-                                 legend.title.var2.txt = var2,
-                                 legend.title.cex = 0.8, legend.values.cex = 0.6,
-                                 legend.style = "a", legend.frame = FALSE,
-                                 legend.values.rnd = 0, add = TRUE){
+                                  var, k = 0.02, fixmax = NULL, symbols = "circle", border = "grey20", lwd = 1,
+                                  var2, col = NULL, nclass=NULL, breaks=NULL, method="quantile",
+                                  legend.var.pos = "right",legend.var2.pos = "topright", 
+                                  legend.var.title.txt = var, legend.var2.title.txt = var2,
+                                  legend.title.cex = 0.8, 
+                                  legend.values.cex = 0.6,
+                                  legend.var.style = "c",
+                                  legend.var.frame = FALSE, legend.var2.frame = FALSE,
+                                  legend.var.values.rnd = 0, legend.var2.values.rnd = 0,
+                                  legend.var2.nodata = "no data",
+                                  add = TRUE){
   if (is.null(spdfid)){spdfid <- names(spdf@data)[1]}
   if (is.null(dfid)){dfid <- names(df)[1]}
   dots <- cbind(spdf@data[,spdfid], as.data.frame(sp::coordinates(spdf)))
-  colnames(dots) <- c(spdfid, "x", "y")
+  colnames(dots)[1] <- c(spdfid)
   dots <- data.frame(dots, df[match(dots[,spdfid], df[,dfid]),])
-  dots <- dots[order(dots[, var], decreasing = TRUE),]
+  dots <- dots[order(abs(dots[, var]), decreasing = TRUE),]
   
   x1 <- sp::bbox(spdf)[1]
   y1 <- sp::bbox(spdf)[2]
@@ -102,91 +95,97 @@ propSymbolsChoroLayer <- function(spdf, df, spdfid = NULL, dfid = NULL,
     dots$heightSize <- abs(dots[, var]) * k * hfdc / fixmax * 10
   }
   
-  # get the colors and distr
-  layer <- choro(var=dots[,var2], distr = distr, col = col,
-                 nclass = nclass, method = method)
-  
-  
-  # CIRCLES
-  if (symbols == "circles"){
-    symbols(dots[, c("x", "y")], circles = dots$circleSize, bg = as.vector(layer$colMap),
-            add = add,
-            inches = FALSE, asp = 1, xlab = "", ylab = "")
-    sizevect <- dots$circleSize
-    varvect <- dots[,var]
-#     LegendCircSymbols(pos = legend.var.pos, legTitle = legend.title.var.txt,
-#                       legTitleCex = legend.title.cex,
-#                       legValuesCex = legend.values.cex,
-#                       varvect = varvect,
-#                       sizevect = sizevect,
-#                       breakval  = NULL,
-#                       round = legend.values.rnd,
-#                       col1 = "white",
-#                       col2 = "white",
-#                       frame = legend.frame,
-#                       type = legend.style)
+  if (add==FALSE){
+    sp::plot(spdf, col = NA, border = NA)
   }
   
+  # get the colors and distr
+  layer <- choro(var=dots[,var2], distr = breaks, col = col,
+                 nclass = nclass, method = method)
+  
+  # CIRCLES
+  if (symbols == "circle"){
+    symbols(dots[, 2:3], circles = dots$circleSize, bg = as.vector(layer$colMap), 
+            fg = border, lwd = lwd,
+            add = TRUE,
+            inches = FALSE, asp = 1	)
+    sizevect <- dots$circleSize
+    varvect <- dots[,var]
+    if(legend.var.pos!="n"){
+      legendCirclesSymbols(pos = legend.var.pos, 
+                           title.txt = legend.var.title.txt,
+                           title.cex = legend.title.cex,
+                           values.cex = legend.values.cex,
+                           var = varvect,
+                           r = sizevect,
+                           col = "grey",
+                           frame = legend.var.frame,
+                           values.rnd =  legend.var.values.rnd,
+                           style = legend.var.style)
+    }
+  }
   # SQUARES
-  if (symbols == "squares"){
-    symbols(dots[, c("x", "y")], squares = dots$squareSize, bg = as.vector(layer$colMap),
-            add = add, inches = FALSE, asp = 1, xlab = "", ylab = "")
+  if (symbols == "square"){
+    symbols(dots[, 2:3], squares = dots$squareSize, bg = as.vector(layer$colMap),
+            fg = border, lwd = lwd,
+            add = TRUE, inches = FALSE, asp = 1)
     sizevect <- dots$squareSize
     varvect <- dots[,var]
-    
-#     LegendSquaresSymbols(pos = legend.var.pos, legTitle = legend.title.var.txt,
-#                       legTitleCex = legend.title.cex,
-#                       legValuesCex = legend.values.cex,
-#                       varvect = varvect,
-#                       sizevect = sizevect,
-#                       breakval  = NULL,
-#                       col1 = "white",
-#                       col2 = "white",
-#                       frame = legend.frame,
-#                       round = legend.values.rnd,
-#                       type = legend.style)
-#     
+    if(legend.var.pos!="n"){
+      legendSquaresSymbols(pos = legend.var.pos, 
+                           title.txt = legend.var.title.txt,
+                           title.cex = legend.title.cex,
+                           values.cex = legend.values.cex,
+                           var = varvect,
+                           r = sizevect,
+                           col = "grey",
+                           frame = legend.var.frame,
+                           values.rnd =  legend.var.values.rnd,
+                           style = legend.var.style)
+    }
   }
   
   #BARRES
-  if (symbols == "height"){
-    width<-min((par()$usr[4]-par()$usr[3])/40,(par()$usr[2]-par()$usr[1])/40)
-    tmp <- as.matrix(data.frame(width,dots$heightSize))
-    dots$y2 <- dots$y+dots$heightSize/2
-    symbols(dots[,c("x","y2")], rectangles = tmp, add = add, bg = as.vector(layer$colMap),
-            inches = FALSE, asp = 1, xlab = "", ylab = "")
+  if (symbols == "bar"){
+    width<-min((par()$usr[4] - par()$usr[3]) / 40, (par()$usr[2] - par()$usr[1]) / 40)
+    tmp <- as.matrix(data.frame(width, dots$heightSize))
+    dots[,3] <- dots[,3] + dots$heightSize / 2
+    symbols(dots[,2:3], rectangles = tmp, add = TRUE, bg = as.vector(layer$colMap),
+            fg = border, lwd = lwd,
+            inches = FALSE, asp = 1)
     sizevect <- dots$heightSize
     varvect <- dots[,var]
-#     
-#     LegendHeightSymbols(pos = legend.var.pos, legTitle = legend.title.var.txt,
-#                         legTitleCex = legend.title.cex,
-#                         legValuesCex = legend.values.cex,
-#                         varvect = varvect,
-#                         sizevect = sizevect,
-#                         breakval  = NULL,
-#                         col1 = "white",
-#                         col2 = "white",
-#                         frame = legend.frame,
-#                         round = legend.values.rnd,
-#                         type = legend.style)
-    
+    if(legend.var.pos!="n"){
+      legendBarsSymbols(pos = legend.var.pos, 
+                        title.txt = legend.var.title.txt,
+                        title.cex = legend.title.cex,
+                        values.cex = legend.values.cex,
+                        var = varvect,
+                        r = sizevect,
+                        col = "grey",
+                        frame = legend.var.frame,
+                        values.rnd =  legend.var.values.rnd,
+                        style = legend.var.style)
+      
+    }
+  }
+  
+  nodata <- FALSE
+  if(max(is.na(dots[,var2])>0)){nodata <- TRUE}
+  if(legend.var2.pos !="n"){
+    legendChoro(pos = legend.var2.pos, 
+                title.txt = legend.var2.title.txt,
+                title.cex = legend.title.cex,
+                values.cex = legend.values.cex,
+                breaks = layer$distr, 
+                col = layer$col, 
+                values.rnd = legend.var2.values.rnd,
+                frame = legend.var2.frame, 
+                symbol="box", 
+                nodata = nodata, 
+                nodata.txt = legend.var2.nodata)
     
   }
-
-# LEGENDE CAISSONS CHORO
-# LegendChoro(pos = legend.var2.pos, 
-#             legTitle = legend.title.var2.txt,
-#             legTitleCex = legend.title.cex,
-#             legValuesCex = legend.values.cex,
-#             distr = layer$distr, 
-#             cols = layer$col, 
-#             round = legend.values.rnd,
-#             frame = legend.frame, 
-#             symbol="box",
-#             nodata = FALSE, 
-#             nodatalabel = "No data available")
-
-
-
   
 }
+
