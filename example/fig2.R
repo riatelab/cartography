6b7a5795a8bc5c1d044c20ae78a81c792e2055e6
neocarto <nicolas.lambert@ums-riate.fr>
2016-10-03 10:30:06 +0200

    funtion to build a map template (on progress)

--- example/fig2.R
+++ example/fig2.R
@@ -1,4 +1,6 @@
 library(cartography)
+
+opar <- par(mar = c(0,0,1.2,0), mfrow = c(1,1))
 # Load data
 data(nuts2006)
 # set margins
@@ -16,10 +18,14 @@ v <- getBreaks(v = df1$cagr ,method = "quantile",nclass = 10)
 v <- v[order(v)]
 v[5] <- 0
 
-cols <- carto.pal(pal1 = "pink.pal", n1 = 4,pal2 = "taupe.pal", n2 = 6, middle = F )
+#cols <- carto.pal(pal1 = "pink.pal", n1 = 4,pal2 = "taupe.pal", n2 = 6, middle = F )
+cols <- c("#f18b61","#f7b48c","#f3d9b7","#f1eccd","#c0dec2","#91caa4","#63b285","#329966","#26734d","#1a4c33")
 
+plot.new()
+sizes <- getFigDim(spdf = nuts0.spdf, width = 18, mar = c(0,0,0,0))
 
-opar <- par(mar = c(0,0,1.2,0))
+png(filename = "RegularGrid", width = sizes[1], height = sizes[2],units = "cm",res = 150)
+
 # Plot a layer with the extent of the EU28 countries with only a background color
 plot(nuts0.spdf, border = NA, col = NA, bg = "#A6CAE0")
 
@@ -27,17 +33,20 @@ plot(nuts0.spdf, border = NA, col = NA, bg = "#A6CAE0")
 plot(world.spdf, col  = "#E3DEBF", border=NA, add=TRUE)
 
 choroLayer(spdf = mygrid$spdf,df = df1,var = "cagr", add=TRUE,
-           col=cols, lwd = 0.5, border = NA,
+           col=cols, lwd = 0.6, border = "#FFFFFF60",
            legend.pos = "right", breaks = v,legend.values.rnd = 2,
-           legend.title.txt = "COUMPOUND")
+           legend.title.txt = "Compound\nAnnual\nGrowth Rate",)
 
 
-plot(nuts0.spdf, add=T, col = NA, border = "grey20", lwd = 1)
+plot(nuts0.spdf, add=T, col = NA, border = "#56514c", lwd = 0.7)
 
 
-layoutLayer(title = "COUMPOUND", author = "cartography", 
+layoutLayer(title = "Demographic Trends, 1999-2008", author = "cartography", 
             sources = "Eurostat, 2008", frame = TRUE, col = "white", 
             scale = 500, coltitle = "black",
             north = TRUE, theme = "taupe.pal") 
+
+dev.off()
+
 
 
