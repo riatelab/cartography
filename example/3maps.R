abb1fd6d0c5e7e773f2594dcf3a337540fa38d75
Timothée Giraud <timothee.giraud@ums-riate.fr>
2016-10-05 15:59:52 +0200

    examples

--- example/3maps.R
+++ example/3maps.R
@@ -3,6 +3,17 @@ library(cartography)
 # Load data
 data(nuts2006)
 
+
+
+sizes <- getFigDim(spdf = nuts0.spdf, width = 400, mar = c(0,0,1.2,0))
+
+sizes[1] <- sizes[1] * 3 
+
+
+png(filename = "example/3maps.png", width = sizes[1], height = sizes[2], res = 150)
+
+?png
+
 # set margins
 opar <- par(mar = c(0,0,1.2,0), mfrow = c(1,3))
 
@@ -13,21 +24,24 @@ plot(nuts0.spdf, border = NA, col = NA, bg = "#A6CAE0")
 plot(world.spdf, col  = "#E3DEBF", border=NA, add=TRUE)
 
 
-plot(nuts0.spdf, col = "#D1914D",border = "white", lwd=1, add=TRUE)
+plot(nuts0.spdf, col = "#D1914D",border = "white", lwd=0.5, add=TRUE)
 
 
 nuts0.df$pop <- nuts0.df$pop2008 / 1000
 
 # Plot the compound annual growth rate
-propSymbolsLayer(spdf = nuts0.spdf, df = nuts0.df, var = "pop", inches = 0.15, 
+propSymbolsLayer(spdf = nuts0.spdf, df = nuts0.df, var = "pop", 
+                 inches = 0.15, lwd = 0.5,
                  legend.pos = "right", legend.values.rnd = 0, 
-                 legend.title.txt = "Population\n(thousands of inh.)", 
+                 legend.title.txt = "Population, 2008\n(thousands of inh.)", 
                  legend.frame = F)
 # Layout plot
-layoutLayer(title = "propSymbolsLayer()", author = "cartography", 
-            sources = "Eurostat, 2008", frame = TRUE, col = "white", 
-            scale = 500, coltitle = "black",
+layoutLayer(title = "\npropSymbolsLayer()",  author = "Package cartography v1.4.0", 
+            sources = "Source: Eurostat, 2011", frame = FALSE,  
+            scale = NULL, theme = "wine.pal",
             north = TRUE) # add a south arrow
+
+
 
 nuts0.df$gdpinh <- nuts0.df$gdppps2008 * 1000000 / nuts0.df$pop2008
 
@@ -40,20 +54,20 @@ plot(world.spdf, col  = "#E3DEBF", border=NA, add=TRUE)
 choroLayer(spdf = nuts0.spdf, df = nuts0.df, var = "gdpinh", 
            col = carto.pal(pal1 = "green.pal", n1 = 4), 
            method = "quantile",nclass = 4,  border = "white", lwd = 0.5,  
-           legend.pos = "right", legend.title.txt = "GDP per inh.\n(euros)", 
+           legend.pos = "right", legend.title.txt = "GDP per inh. ,2008\n(euros)", 
            add=T)
 
-layoutLayer(title = "choroLayer()", author = "cartography", 
-            sources = "Eurostat, 2008", frame = TRUE, col = "white", 
-            scale = 500, coltitle = "black",
-            north = TRUE) 
+layoutLayer(title = "\nchoroLayer()", author = "", 
+            sources = "", frame = FALSE, 
+            scale = NULL, theme = "green.pal",
+            north = FALSE) 
 
 # Plot a layer with the extent of the EU28 countries with only a background color
 plot(nuts0.spdf, border = NA, col = NA, bg = "#A6CAE0")
 
 # Plot non european space
 plot(world.spdf, col  = "#E3DEBF", border=NA, add=TRUE)
-plot(nuts0.spdf, col = "#D1914D",border = "white", lwd=1, add=TRUE)
+plot(nuts0.spdf, col = "#F8FD4050",border = "white", lwd=0.5, add=TRUE)
 # Plot symbols with choropleth coloration
 propSymbolsChoroLayer(spdf = nuts0.spdf, 
                       df = nuts0.df, 
@@ -67,21 +81,21 @@ propSymbolsChoroLayer(spdf = nuts0.spdf,
                       lwd = 0.5, # symbols width
                       legend.var.pos = "topright", # size legend position
                       legend.var.values.rnd = 0, # size legend value roundinf
-                      legend.var.title.txt = "Population\n(thousands of inh.)", # size legend title
+                      legend.var.title.txt = "Population, 2008\n(thousands of inh.)", # size legend title
                       legend.var.style = "e", # size legend type
                       legend.var2.pos = "right", # color legend position
                       legend.var2.values.rnd = 0,
-                      legend.var2.title.txt = "GDP per inh.\n(euros)") # legend title
+                      legend.var2.title.txt = "GDP per inh., 2008\n(euros)") # legend title
 
 
 # layout
-layoutLayer(title = "propSymbolsChoroLayer()", author = "cartography", 
-            sources = "Eurostat, 2008", frame = TRUE, col = "white", 
-            scale = 500, coltitle = "black",
-            north = TRUE) 
+layoutLayer(title = "\npropSymbolsChoroLayer()", author = "", 
+            sources = "", frame = FALSE,  theme = "blue.pal",
+            scale = 500, 
+            north = FALSE) 
 
 
 
 
-# dev.off()
+dev.off()
 
--- example/RegularGrid.R
+++ example/RegularGrid.R
@@ -1,12 +1,9 @@
-cartography.colors
-
 data("nuts2006")
 library(cartography)
 
-opar <- par(mar = c(0,0,1.2,0), mfrow = c(1,1))
 # Load data
 data(nuts2006)
-# set margins
+
 
 # Create a grid layer
 mygrid <- getGridLayer(spdf = nuts3.spdf, cellsize = 100000, type = "hexagonal")
@@ -18,40 +15,41 @@ df2 <- getGridData(x = mygrid, df = nuts3.df, var = "pop1999" )
 # Compute the compound annual growth rate
 df1$cagr <-(((df1$pop2008 / df2$pop1999)^(1/9)) - 1) * 100
 v <- getBreaks(v = df1$cagr ,method = "quantile",nclass = 10)
-v <- v[order(v)]
 v[5] <- 0
 
+# set a color palette
+cols <- c("#f18b61","#f7b48c","#f3d9b7","#f1eccd","#c0dec2",
+          "#91caa4","#63b285","#329966","#26734d","#1a4c33")
 
-#cols <- carto.pal(pal1 = "pink.pal", n1 = 4,pal2 = "taupe.pal", n2 = 6, middle = F )
-cols <- c("#f18b61","#f7b48c","#f3d9b7","#f1eccd","#c0dec2","#91caa4","#63b285","#329966","#26734d","#1a4c33")
 
-plot.new()
-sizes <- getFigDim(spdf = nuts0.spdf, width = 18, mar = c(0,0,0,0))
+sizes <- getFigDim(spdf = nuts0.spdf, width = 1200, mar = c(0,0,1.2,0))
 
-# png(filename = "RegularGrid", width = sizes[1], height = sizes[2],units = "cm",res = 150)
+png(filename = "example/RegularGrid.png", width = sizes[1], height = sizes[2],res = 150)
 
+# set margins
+opar <- par(mar = c(0,0,1.2,0), mfrow = c(1,1))
 
 # Plot a layer with the extent of the EU28 countries with only a background color
 plot(nuts0.spdf, border = NA, col = NA, bg = "#A6CAE0")
 
 # Plot non european space
-plot(world.spdf, col  = "#E3DEBF", border=NA, add=TRUE)
+plot(world.spdf, col  = "#CCCCCC", border=NA, add=TRUE)
 
 choroLayer(spdf = mygrid$spdf,df = df1,var = "cagr", add=TRUE,
            col=cols, lwd = 0.6, border = "#FFFFFF60",
            legend.pos = "right", breaks = v,legend.values.rnd = 2,
-           legend.title.txt = "Compound\nAnnual\nGrowth Rate",)
+           legend.title.txt = "Compound Annual\nGrowth Rate")
 
 
 plot(nuts0.spdf, add=T, col = NA, border = "#56514c", lwd = 0.7)
 
 
-layoutLayer(title = "Demographic Trends, 1999-2008", author = "cartography", 
-            sources = "Eurostat, 2008", frame = TRUE, col = "white", 
-            scale = 500, coltitle = "black",
+layoutLayer(title = "Demographic Trends, 1999-2008", author = "Package cartography v1.4.0", 
+            sources = "Source: Eurostat, 2011", frame = TRUE, 
+            scale = 500, 
             north = TRUE, theme = "taupe.pal") 
 
-# dev.off()
+dev.off()
 
 
 
