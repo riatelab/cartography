8c719a57102d00b2b2a4e58249993af6941ebe74
rcarto <timothee.giraud@ums-riate.fr>
2017-04-06 17:49:43 +0200

    getBordersSF

--- R/getBorders.R
+++ R/getBorders.R
@@ -1,10 +1,8 @@
 #' @title Extract SpatialPolygonsDataFrame Borders
 #' @description Extract borders between SpatialPolygonsDataFrame units.
 #' @name getBorders
-#' @param spdf a SpatialPolygonsDataFrame. This SpatialPolygonsDataFrame
-#'  has to be projected (planar coordinates).
-#' @param spdfid identifier field in spdf, default to the first column 
-#' of the spdf data frame. (optional)
+#' @param x an sf object, a simple feature collection (or a SpatialPolygonsDataFrame).
+#' @param id identifier field in x, default to the first column. (optional)
 #' @note getBorders and getOuterBorders can be combined with rbind. 
 #' @return A SpatialLinesDataFrame of borders is returned. This object has three 
 #' id fields: id, id1 and id2.
@@ -13,102 +11,72 @@
 #' @examples
 #' data(nuts2006)
 #' # Get units borders
-#' nuts0.contig.spdf <- getBorders(nuts0.spdf)
+#' nuts0.contig.spdf <- getBorders(st_as_sf(nuts0.spdf))
 #' # Plot Countries
 #' plot(nuts0.spdf, border = NA, col = "grey60")
 #' # Plot borders
-#' plot(nuts0.contig.spdf, 
+#' plot(st_geometry(nuts0.contig.spdf), 
 #'      col = sample(x = rainbow(nrow(nuts0.contig.spdf))), 
 #'      lwd = 3, add = TRUE)
 #' @seealso \link{discLayer}, \link{getOuterBorders}
 #' @import rgeos
 #' @import sp
+#' @import sf
 #' @export
-getBorders <- function(spdf, spdfid = NULL){
+getBorders <- function(x, id = NULL){
+  # library(sf)
+  # data(nuts2006)
+  # 
+  # x = st_as_sf(nuts0.spdf)
+  # id = NULL
   
-  id <- spdfid
-  if (is.null(id)) {
-    id <- names(spdf@data)[1]
-  }
-  spdf@data <- spdf@data[id]
-  colnames(spdf@data)[1] <- "id"
-  row.names(spdf) <- as.character(spdf@data$id)
+  if(is.null(id)){id <- names(x)[1]}
   
-  spdf <- gBuffer(spdf, width = 1, byid = T)
-  
-  df <- data.frame(id = sapply(methods::slot(spdf, "polygons"),
-                               methods::slot, "ID"))
-  row.names(df) <- df$id
-  spdf <- SpatialPolygonsDataFrame(Sr = spdf, data = df)
-  sldf <- methods::as(object = spdf, Class = "SpatialLines")
-  df <- data.frame(id = sapply(methods::slot(sldf, "lines"),
-                               methods::slot, "ID"))
-  row.names(df) <- df$id
-  sldf <- SpatialLinesDataFrame(sl = sldf, data = df)
-  l <-gIntersects(spdf,
-                  checkValidity = TRUE,
-                  byid = T,
-                  returnDense = T)
+  st_geometry(x) <-  st_buffer(st_geometry(x), 1)
+  l <- st_intersects(x,x, sparse = F)
+  colnames(l) <- x[[id]]
+  rownames(l) <- x[[id]]
   l <- lower.tri(l) * l
+  
   gna <- function(x) {
     y <- x[x == 1]
     if (length(y) > 0) {
       names(y)
-    } else{
+    } else {
       NA
     }
   }
   myl <- as.list(apply(l, 1, gna))
   myl <- myl[!is.na(myl)]
-  
   long <- sum(sapply(myl, length))
-  df <- data.frame(
-    id = rep(NA, long),
-    id1 = rep(NA, long),
-    id2 = rep(NA, long)
-  )
+  df <- data.frame(id = rep(NA, long), id1 = rep(NA, long), id2 = rep(NA, long))
+  
+  lgeo <- vector(mode = "list", length = long)
+  lgeo2 <- vector(mode = "list", length = long)
   ind <- 1
-  wkt <- rep(NA, long)
+  i<-1
   for (i in 1:length(myl)) {
     id1 <- names(myl[i])
-    li <- sldf[sldf$id == id1, ]
+    li <- x[x[[id]] == id1, ]
+    j <- 1
     for (j in 1:length(myl[[i]])) {
       id2 <- myl[[i]][[j]]
-      po <- spdf[spdf$id == id2, ]
-      Inter <- rgeos::gIntersection(li, po, byid = T)
-      if (class(Inter) != "SpatialLines") {
-        if (class(Inter) == "SpatialPoints") {
-          rm(Inter)
-        } else{
-          Inter <- Inter@lineobj
-          row.names(Inter) <- paste(id1, id2, sep = "_")
-          wkt[ind] <- rgeos::writeWKT(Inter)
-          df[ind,] <- c(paste(id1, id2, sep = "_"), id1, id2)
-          ind <- ind + 1
-        }
-      } else{
-        row.names(Inter) <- paste(id1, id2, sep = "_")
-        wkt[ind] <- rgeos::writeWKT(Inter)
-        df[ind,] <- c(paste(id1, id2, sep = "_"), id1, id2)
-        ind <- ind + 1
-      }
+      po <- x[x[[id]] == id2, ]
+      Inter <- st_intersection(st_geometry(li), st_geometry(po))
+      df[ind,] <- c(paste0(id1,"_",id2), id1, id2)
+      lgeo[[ind]]   <- Inter[[1]]
+      ind <- ind + 1
     }
   }
-  wkt <- wkt[!is.na(wkt)]
-  wkt <- paste("GEOMETRYCOLLECTION(",
-               paste(wkt, collapse = ","), ")", sep = "")
+  df <- st_sf(df, geometry = st_sfc(lgeo))
+  df <-  st_cast(x = df, to = "MULTILINESTRING")
+  st_crs(df) <- st_crs(x)
   
-  b <- readWKT(wkt, id = df$id)
-  row.names(df) <- df$id
-  b <- SpatialLinesDataFrame(sl = b,
-                             data = df,
-                             match.ID = T)
-  b2 <- b
-  b2@data <- b2@data[, c(1, 3, 2)]
-  names(b2) <- c("id", "id1", "id2")
-  b2$id <- paste(b2$id1, b2$id2, sep = "_")
-  row.names(b2) <- b2$id
-  borderlines <- rbind(b, b2)
-  borderlines@proj4string <- sldf@proj4string
+  df2 <- df[, c(1,3,2)]
+  
+  names(df2) <- c("id", "id1", "id2", "geometry")
+  df2$id <- paste(df2$id1, df2$id2, sep = "_")
+  borderlines <- rbind(df, df2)
+  row.names(borderlines) <- borderlines$id
   return(borderlines)
 }
