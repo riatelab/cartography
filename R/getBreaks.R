23f1df2a1e0fc60d8b80043cd4c0b8dcb1abdb8e
rcarto <timothee.giraud@ums-riate.fr>
2017-04-18 17:44:08 +0200

    add 2 methods to getBreaks

--- R/getBreaks.R
+++ R/getBreaks.R
@@ -4,7 +4,7 @@
 #' @param v a vector of numeric values.
 #' @param nclass a number of classes
 #' @param method a discretization method; one of "sd", "equal", 
-#' "quantile", "fisher-jenks","q6", "geom"  or "msd" (see Details).
+#' "quantile", "fisher-jenks","q6", "geom", "arith", "em" or "msd" (see Details).
 #' @param k number of standard deviation for "msd" method (see Details)..
 #' @param middle creation of a central class for "msd" method (see Details). 
 #' @details 
@@ -13,6 +13,8 @@
 #' quite similar results but Fisher-Jenks is much faster. \cr\cr
 #' The "q6" method uses the following \link{quantile} probabilities: 0, 0.05, 0.275, 0.5, 0.725, 0.95, 1.\cr\cr   
 #' The "geom" method is based on a geometric progression along the variable values.\cr\cr
+#' The "arith" method is based on an arithmetic progression along the variable values.\cr\cr
+#' The "em" method is based on nested averages computation.\cr\cr 
 #' The "msd" method is based on the mean and the standard deviation of a numeric vector. 
 #' The nclass parameter is not relevant, use k and middle instead. k indicates 
 #' the extent of each class in share of standard deviation. If middle=TRUE then 
@@ -82,12 +84,51 @@ getBreaks <- function(v, nclass = NULL, method = "quantile",
         intervals <- sort(intervals)
       }
     }
+    if (method=="arith")
+    {
+      intervals <- min(v)
+      intervals <- c(intervals,max(v))
+      r <- (max(v) - min(v)) / sum(1:nclass) # raison
+      tmp <- min(v)
+      for ( i in 1:(nclass-1)) {
+        intervals <- c(intervals,tmp+r)
+        tmp <- tmp+r
+        intervals <- sort(intervals)
+      }
+    }
     if (method == "q6")
     {
       intervals <- as.vector(quantile(v,probs = c(0, 5, 27.5, 50, 72.5, 95, 100)/100))
     }
+    if (method == "em"){
+      t <- bitwAnd(nclass,(nclass-1))
+      if(t != 0){
+        stop("The number of classes must be a power of 2")
+      }else{
+        minVec <- min(v)
+        maxVec <- max(v)
+        it <- log2(nclass)
+        
+        int <- c(minVec,maxVec)
+        
+        for (a in 1:it){
+          valprv <- c()
+          for (i in 1:(length(int)-1)){
+            if (i == 1){
+              subVec <- v[v >= int[i] & v <= int[i+1]]
+            }else{
+              subVec <- v[v > int[i] & v <= int[i+1]]
+            }
+            valprv <- c(valprv, mean(subVec))
+          }
+          int <- c(int, valprv)
+          int <-int[order(int)]
+        }
+        intervals <- int
+      }
+    }
     if (method == "msd"){
-
+      
       minVec <- min(v)
       maxVec <- max(v)
       avgVec <- mean(v)
@@ -106,9 +147,9 @@ getBreaks <- function(v, nclass = NULL, method = "quantile",
         pose <- ceiling((maxVec - (avgVec + 0.5 * sdVec * k)) / (sdVec * k))
         nege <- ceiling(((avgVec - 0.5 * sdVec * k ) - minVec) / (sdVec *k))
         
-        bks <- c((avgVec -  0.5 * sdVec * k) - (1:nege) * (sdVec * k), 
-                 (avgVec -  0.5 * sdVec * k), 
-                 (avgVec +  0.5 * sdVec * k), 
+        bks <- c((avgVec - 0.5 * sdVec * k) - (1:nege) * (sdVec * k), 
+                 (avgVec - 0.5 * sdVec * k), 
+                 (avgVec + 0.5 * sdVec * k), 
                  (avgVec + 0.5 * sdVec * k) + (1:pose) * (sdVec * k))
         intervals <- c(minVec, bks[bks> minVec & bks <maxVec], maxVec)
       }
@@ -117,4 +158,3 @@ getBreaks <- function(v, nclass = NULL, method = "quantile",
   }
   return(intervals)
 }
-
