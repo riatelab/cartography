7e09ed7e6cdaa2d3f38d57a3c6c296c6347c4636
rcarto <timothee.giraud@ums-riate.fr>
2017-04-19 14:27:57 +0200

    revert changes, sf support, internal is managed with sp

--- R/getBorders.R
+++ R/getBorders.R
@@ -1,12 +1,12 @@
 #' @title Extract SpatialPolygonsDataFrame Borders
 #' @description Extract borders between SpatialPolygonsDataFrame units.
 #' @name getBorders
-#' @param x an sf object, a simple feature collection (or a SpatialPolygonsDataFrame).
+#' @param x an sf object, a simple feature collection.
 #' @param spdf a SpatialPolygonsDataFrame. This SpatialPolygonsDataFrame
 #'  has to be projected (planar coordinates).
 #' @param id identifier field in x or spdf, default to the first column. (optional)
 #' @note getBorders and getOuterBorders can be combined with rbind. 
-#' @return A sf object (MULTILINESTRING) of borders is returned. This object has three 
+#' @return An sf object (MULTILINESTRING) of borders is returned. This object has three 
 #' id fields: id, id1 and id2.
 #' id1 and id2 are ids of units that neighbour a border; id is the concatenation 
 #' of id1 and id2 (with "_" as separator).
@@ -20,64 +20,155 @@
 #' plot(st_geometry(nuts0.contig), 
 #'      col = sample(x = rainbow(nrow(nuts0.contig))), 
 #'      lwd = 3, add = TRUE)
-#' @seealso \link{discLayer}
+#' @seealso \link{discLayer}, \link{getOuterBorders}
 #' @import rgeos
 #' @import sp
 #' @import sf
 #' @export
 getBorders <- function(x, spdf, id = NULL){
-
-  if(missing(x)){
-    x <- sf::st_as_sf(spdf)
+  if(!missing(x)){spdf <- methods::as(x, "Spatial")}
+  
+  if (is.null(id)) {
+    id <- names(spdf@data)[1]
   }
+  spdf@data <- spdf@data[id]
+  colnames(spdf@data)[1] <- "id"
+  row.names(spdf) <- as.character(spdf@data$id)
   
-  if(is.null(id)){id <- names(x)[1]}
+  spdf <- gBuffer(spdf, width = 1, byid = T)
   
-  st_geometry(x) <-  st_buffer(st_geometry(x), 1)
-  l <- st_intersects(x,x, sparse = F)
-  colnames(l) <- x[[id]]
-  rownames(l) <- x[[id]]
+  df <- data.frame(id = sapply(methods::slot(spdf, "polygons"),
+                               methods::slot, "ID"))
+  row.names(df) <- df$id
+  spdf <- SpatialPolygonsDataFrame(Sr = spdf, data = df)
+  sldf <- methods::as(object = spdf, Class = "SpatialLines")
+  df <- data.frame(id = sapply(methods::slot(sldf, "lines"),
+                               methods::slot, "ID"))
+  row.names(df) <- df$id
+  sldf <- SpatialLinesDataFrame(sl = sldf, data = df)
+  l <-gIntersects(spdf,
+                  checkValidity = TRUE,
+                  byid = T,
+                  returnDense = T)
   l <- lower.tri(l) * l
-  
   gna <- function(x) {
     y <- x[x == 1]
     if (length(y) > 0) {
       names(y)
-    } else {
+    } else{
       NA
     }
   }
   myl <- as.list(apply(l, 1, gna))
   myl <- myl[!is.na(myl)]
-  long <- sum(sapply(myl, length))
-  df <- data.frame(id = rep(NA, long), id1 = rep(NA, long), id2 = rep(NA, long))
   
-  lgeo <- vector(mode = "list", length = long)
-  lgeo2 <- vector(mode = "list", length = long)
+  long <- sum(sapply(myl, length))
+  df <- data.frame(
+    id = rep(NA, long),
+    id1 = rep(NA, long),
+    id2 = rep(NA, long)
+  )
   ind <- 1
-  i<-1
+  wkt <- rep(NA, long)
   for (i in 1:length(myl)) {
     id1 <- names(myl[i])
-    li <- x[x[[id]] == id1, ]
-    j <- 1
+    li <- sldf[sldf$id == id1, ]
     for (j in 1:length(myl[[i]])) {
       id2 <- myl[[i]][[j]]
-      po <- x[x[[id]] == id2, ]
-      Inter <- st_intersection(st_geometry(li), st_geometry(po))
-      df[ind,] <- c(paste0(id1,"_",id2), id1, id2)
-      lgeo[[ind]]   <- Inter[[1]]
-      ind <- ind + 1
+      po <- spdf[spdf$id == id2, ]
+      Inter <- rgeos::gIntersection(li, po, byid = T)
+      if (class(Inter) != "SpatialLines") {
+        if (class(Inter) == "SpatialPoints") {
+          rm(Inter)
+        } else{
+          Inter <- Inter@lineobj
+          row.names(Inter) <- paste(id1, id2, sep = "_")
+          wkt[ind] <- rgeos::writeWKT(Inter)
+          df[ind,] <- c(paste(id1, id2, sep = "_"), id1, id2)
+          ind <- ind + 1
+        }
+      } else{
+        row.names(Inter) <- paste(id1, id2, sep = "_")
+        wkt[ind] <- rgeos::writeWKT(Inter)
+        df[ind,] <- c(paste(id1, id2, sep = "_"), id1, id2)
+        ind <- ind + 1
+      }
     }
   }
-  df <- st_sf(df, geometry = st_sfc(lgeo))
-  df <-  st_cast(x = df, to = "MULTILINESTRING")
-  st_crs(df) <- st_crs(x)
+  wkt <- wkt[!is.na(wkt)]
+  wkt <- paste("GEOMETRYCOLLECTION(",
+               paste(wkt, collapse = ","), ")", sep = "")
   
-  df2 <- df[, c(1,3,2)]
+  b <- readWKT(wkt, id = df$id)
+  row.names(df) <- df$id
+  b <- SpatialLinesDataFrame(sl = b,
+                             data = df,
+                             match.ID = T)
+  b2 <- b
+  b2@data <- b2@data[, c(1, 3, 2)]
+  names(b2) <- c("id", "id1", "id2")
+  b2$id <- paste(b2$id1, b2$id2, sep = "_")
+  row.names(b2) <- b2$id
+  borderlines <- rbind(b, b2)
+  borderlines@proj4string <- sldf@proj4string
   
-  names(df2) <- c("id", "id1", "id2", "geometry")
-  df2$id <- paste(df2$id1, df2$id2, sep = "_")
-  borderlines <- rbind(df, df2)
-  row.names(borderlines) <- borderlines$id
+  borderlines <- sf::st_as_sf(borderlines)
+  
   return(borderlines)
 }
+  
+#   
+#   if(missing(x)){
+#     x <- sf::st_as_sf(spdf)
+#   }
+#   
+#   if(is.null(id)){id <- names(x)[1]}
+#   
+#   st_geometry(x) <-  st_buffer(st_geometry(x), 1)
+#   l <- st_intersects(x,x, sparse = F)
+#   colnames(l) <- x[[id]]
+#   rownames(l) <- x[[id]]
+#   l <- lower.tri(l) * l
+#   
+#   gna <- function(x) {
+#     y <- x[x == 1]
+#     if (length(y) > 0) {
+#       names(y)
+#     } else {
+#       NA
+#     }
+#   }
+#   myl <- as.list(apply(l, 1, gna))
+#   myl <- myl[!is.na(myl)]
+#   long <- sum(sapply(myl, length))
+#   df <- data.frame(id = rep(NA, long), id1 = rep(NA, long), id2 = rep(NA, long))
+#   
+#   lgeo <- vector(mode = "list", length = long)
+#   lgeo2 <- vector(mode = "list", length = long)
+#   ind <- 1
+#   i<-1
+#   for (i in 1:length(myl)) {
+#     id1 <- names(myl[i])
+#     li <- x[x[[id]] == id1, ]
+#     j <- 1
+#     for (j in 1:length(myl[[i]])) {
+#       id2 <- myl[[i]][[j]]
+#       po <- x[x[[id]] == id2, ]
+#       Inter <- st_intersection(st_geometry(li), st_geometry(po))
+#       df[ind,] <- c(paste0(id1,"_",id2), id1, id2)
+#       lgeo[[ind]]   <- Inter[[1]]
+#       ind <- ind + 1
+#     }
+#   }
+#   df <- st_sf(df, geometry = st_sfc(lgeo))
+#   df <-  st_cast(x = df, to = "MULTILINESTRING")
+#   st_crs(df) <- st_crs(x)
+#   
+#   df2 <- df[, c(1,3,2)]
+#   
+#   names(df2) <- c("id", "id1", "id2", "geometry")
+#   df2$id <- paste(df2$id1, df2$id2, sep = "_")
+#   borderlines <- rbind(df, df2)
+#   row.names(borderlines) <- borderlines$id
+#   return(borderlines)
+# }
--- R/getOuterBorders.R
+++ R/getOuterBorders.R
@@ -1,111 +1,237 @@
-# #' @title Extract SpatialPolygonsDataFrame Outer Borders
-# #' @description Extract outer borders between SpatialPolygonsDataFrame units. 
-# #' Outer borders are non-contiguous SpatialPolygonsDataFrame borders (e.g. 
-# #' maritim borders).
-# #' @name getOuterBorders
-# #' @param spdf a SpatialPolygonsDataFrame. 
-# #' @param spdfid identifier field in spdf, default to the first column 
-# #' of the spdf data frame. (optional)
-# #' @param res resolution of the grid used to compute borders (in spdf units). 
-# #' A high resolution will give more detailed borders. (optional)
-# #' @param width maximum distance between used to compute borders (in spdf units). 
-# #' A higher width will build borders between units that are farther apart. (optional)
-# #' @return A SpatialLinesDataFrame of borders is returned. This object has three 
-# #' id fields: id, id1 and id2.
-# #' id1 and id2 are ids of units that neighbour a border; id is the concatenation 
-# #' of id1 and id2 (with "_" as separator).
-# #' @note getBorders and getOuterBorders can be combined with rbind. 
-# #' @examples
-# #' \dontrun{
-# #' data(nuts2006)
-# #' # Get units borders
-# #' nuts0.outer <- getOuterBorders(nuts0.spdf)
-# #' # Plot Countries
-# #' plot(nuts0.spdf, border = NA, col = "grey60")
-# #' # Plot borders
-# #' plot(nuts0.outer, col = sample(x = rainbow(nrow(nuts0.outer))), 
-# #'      lwd = 3, add = TRUE)
-# #' }
-# #' @seealso \link{discLayer}, \link{getBorders} 
-# #' @export
-# getOuterBorders <- function(spdf, 
-#                             spdfid = NULL, 
-#                             res = NULL, 
-#                             width = NULL){
-#   
-#   if (is.null(spdfid)) {
-#     spdfid <- names(spdf@data)[1]
-#   }
-#   if(!is.numeric(spdf[,spdfid])){
-#     spdf$idxd <- 1:nrow(spdf)
-#   }else{
-#     spdf$idxd <- spdf@data[, spdfid]
-#   }
-#   
-#   
-#   boundingBox <- sp::bbox(spdf)
-#   w <- (boundingBox[1,2] - boundingBox[1,1]) 
-#   h <- (boundingBox[2,2] - boundingBox[2,1])
-#   
-#   
-#   if(is.null(res)){
-#     res <- round(max(c(w, h))/150,0)
-#   }
+#' @title Extract SpatialPolygonsDataFrame Outer Borders
+#' @description Extract outer borders between SpatialPolygonsDataFrame units.
+#' Outer borders are non-contiguous SpatialPolygonsDataFrame borders (e.g.
+#' maritim borders).
+#' @name getOuterBorders
+#' @param x an sf object, a simple feature collection.
+#' @param spdf a SpatialPolygonsDataFrame. This SpatialPolygonsDataFrame
+#'  has to be projected (planar coordinates).
+#' @param id identifier field in x or spdf, default to the first column. (optional)
+#' @param res resolution of the grid used to compute borders (in spdf units).
+#' A high resolution will give more detailed borders. (optional)
+#' @param width maximum distance between used to compute borders (in spdf units).
+#' A higher width will build borders between units that are farther apart. (optional)
+#' @return A SpatialLinesDataFrame of borders is returned. This object has three
+#' id fields: id, id1 and id2.
+#' id1 and id2 are ids of units that neighbour a border; id is the concatenation
+#' of id1 and id2 (with "_" as separator).
+#' @note getBorders and getOuterBorders can be combined with rbind.
+#' @examples
+#' \dontrun{
+#' data(nuts2006)
+#' # Get units borders
+#' nuts0.outer <- getOuterBorders(nuts0.spdf)
+#' # Plot Countries
+#' plot(nuts0.spdf, border = NA, col = "grey60")
+#' # Plot borders
+#' plot(nuts0.outer, col = sample(x = rainbow(nrow(nuts0.outer))),
+#'      lwd = 3, add = TRUE)
+#' }
+#' @seealso \link{discLayer}, \link{getBorders}
+#' @export
+getOuterBorders <- function(x, spdf, id = NULL, res = NULL, width = NULL){
+  
+  if(!missing(x)){spdf <- methods::as(x, "Spatial")}
+  
+  
+  if (is.null(id)) {
+    id <- names(spdf@data)[1]
+  }
+  if(!is.numeric(spdf[,id])){
+    spdf$idxd <- 1:nrow(spdf)
+  }else{
+    spdf$idxd <- spdf@data[, id]
+  }
+  
+  
+  boundingBox <- sp::bbox(spdf)
+  w <- (boundingBox[1,2] - boundingBox[1,1]) 
+  h <- (boundingBox[2,2] - boundingBox[2,1])
+  
+  
+  if(is.null(res)){
+    res <- round(max(c(w, h))/150,0)
+  }
+  
+  if(is.null(width)){
+    width <- round(max(c(w, h))/20,0)
+  }
+  
+  
+  # Create raster of spdf
+  ex <- raster::extent(spdf)
+  ex[1] <- ex[1] - width
+  ex[2] <- ex[2] + width
+  ex[3] <- ex[3] - width
+  ex[4] <- ex[4] + width
+  
+  r <- raster::raster(ex, resolution = res)
+  
+  r <- raster::rasterize( x= spdf,  y=r, field = 'idxd')
+  
+  dist <- raster::distance(r)  
+  dist[dist > width] <- NA
+  # you can also set a maximum distance: dist[dist > maxdist] <- NA
+  direct <- raster::direction(r, from=FALSE)
+  
+  # NA raster
+  rna <- is.na(r) # returns NA raster
+  
+  # store coordinates in new raster: http://stackoverflow.com/a/35592230/3752258 
+  na.x <- raster::init(rna, 'x')
+  na.y <- raster::init(rna, 'y')
+  
+  
+  # calculate coordinates of the nearest Non-NA pixel
+  # assume that we have a orthogonal, projected CRS, so we can use (Pythagorean) calculations
+  co.x <- na.x + dist * sin(direct)
+  co.y <- na.y + dist * cos(direct)
+  
+  # matrix with point coordinates of nearest non-NA pixel
+  co <- cbind(co.x[], co.y[]) 
+  
+  
+  # extract values of nearest non-NA cell with coordinates co
+  NAVals <- raster::extract(r, co, method='simple') 
+  r.NAVals <- rna # initiate new raster
+  r.NAVals[] <- NAVals # store values in raster
+  
+  pB <- raster::rasterToPolygons(r.NAVals, dissolve = T)
+  pB@data$id <- spdf@data[pB@data$layer, id]
+  pBBorder <- getBorders(spdf = pB, id = "id" )
+  pBBorder <- methods::as(pBBorder, "Spatial")
+  pBBorderS <- rgeos::gSimplify(spgeom = pBBorder, tol = res)
+  
+  result <- sp::SpatialLinesDataFrame(sl = pBBorderS, data = pBBorder@data, match.ID = F)
+  
+  row.names(result) <- paste0(result$id, "_o")
+  result <- sf::st_as_sf(result)
+  return(result)                           
+}                            
+
+
+
+
+
 # 
-#   if(is.null(width)){
-#     width <- round(max(c(w, h))/20,0)
-#   }
-#   
-#   
-#   # Create raster of spdf
-#   ex <- raster::extent(spdf)
-#   ex[1] <- ex[1] - width
-#   ex[2] <- ex[2] + width
-#   ex[3] <- ex[3] - width
-#   ex[4] <- ex[4] + width
-#   
-#   r <- raster::raster(ex, resolution = res)
-#   
-#   r <- raster::rasterize( x= spdf,  y=r, field = 'idxd')
-#   
-#   dist <- raster::distance(r)  
-#   dist[dist > width] <- NA
-#   # you can also set a maximum distance: dist[dist > maxdist] <- NA
-#   direct <- raster::direction(r, from=FALSE)
-#   
-#   # NA raster
-#   rna <- is.na(r) # returns NA raster
-#   
-#   # store coordinates in new raster: http://stackoverflow.com/a/35592230/3752258 
-#   na.x <- raster::init(rna, 'x')
-#   na.y <- raster::init(rna, 'y')
-#   
-#   
-#   # calculate coordinates of the nearest Non-NA pixel
-#   # assume that we have a orthogonal, projected CRS, so we can use (Pythagorean) calculations
-#   co.x <- na.x + dist * sin(direct)
-#   co.y <- na.y + dist * cos(direct)
-#   
-#   # matrix with point coordinates of nearest non-NA pixel
-#   co <- cbind(co.x[], co.y[]) 
-#   
-#   
-#   # extract values of nearest non-NA cell with coordinates co
-#   NAVals <- raster::extract(r, co, method='simple') 
-#   r.NAVals <- rna # initiate new raster
-#   r.NAVals[] <- NAVals # store values in raster
-#   
-#   pB <- raster::rasterToPolygons(r.NAVals, dissolve = T)
-#   pB@data$id <- spdf@data[pB@data$layer, spdfid]
-#   pBBorder <- getBorders(pB, spdfid = "id" )
-#   
-#   pBBorderS <- rgeos::gSimplify(spgeom = pBBorder, tol = res)
-#   
-#   result <- sp::SpatialLinesDataFrame(sl = pBBorderS, data = pBBorder@data)
-#   
-#   row.names(result) <- paste0(row.names(result), "_o")
 # 
-#   return(result)                           
-# }                            
+# x <- sf::st_as_sf(nuts0.spdf)
+# id = NULL
+# res = 10000
+# width = NULL
+# 
+# if(!missing(x)){spdf <- methods::as(x, "Spatial")}
+# 
+# if (is.null(id)) {
+#   id <- names(spdf@data)[1]
+# }
+# if(!is.numeric(spdf[,id])){
+#   spdf$idxd <- 1:nrow(spdf)
+# }else{
+#   spdf$idxd <- spdf@data[, id]
+# }
 # 
 # 
+# boundingBox <- sp::bbox(spdf)
+# w <- (boundingBox[1,2] - boundingBox[1,1])
+# h <- (boundingBox[2,2] - boundingBox[2,1])
+# 
+# 
+# if(is.null(res)){
+#   res <- round(max(c(w, h))/150,0)
+# }
+# 
+# if(is.null(width)){
+#   width <- round(max(c(w, h))/20,0)
+# }
+# 
+# 
+# # Create raster of spdf
+# ex <- raster::extent(spdf)
+# ex[1] <- ex[1] - width
+# ex[2] <- ex[2] + width
+# ex[3] <- ex[3] - width
+# ex[4] <- ex[4] + width
+# 
+# r <- raster::raster(ex, resolution = res)
+# 
+# r <- raster::rasterize( x= spdf,  y=r, field = 'idxd')
+# 
+# dist <- raster::distance(r)
+# dist[dist > width] <- NA
+# # you can also set a maximum distance: dist[dist > maxdist] <- NA
+# direct <- raster::direction(r, from=FALSE)
+# 
+# # NA raster
+# rna <- is.na(r) # returns NA raster
+# 
+# # store coordinates in new raster: http://stackoverflow.com/a/35592230/3752258
+# na.x <- raster::init(rna, 'x')
+# na.y <- raster::init(rna, 'y')
+# 
+# 
+# # calculate coordinates of the nearest Non-NA pixel
+# # assume that we have a orthogonal, projected CRS, so we can use (Pythagorean) calculations
+# co.x <- na.x + dist * sin(direct)
+# co.y <- na.y + dist * cos(direct)
+# 
+# # matrix with point coordinates of nearest non-NA pixel
+# co <- cbind(co.x[], co.y[])
+# 
+# 
+# # extract values of nearest non-NA cell with coordinates co
+# NAVals <- raster::extract(r, co, method='simple')
+# r.NAVals <- rna # initiate new raster
+# r.NAVals[] <- NAVals # store values in raster
+# 
+# pB <- raster::rasterToPolygons(r.NAVals, dissolve = T)
+# pB@data$id <- spdf@data[pB@data$layer, id]
+# 
+# plot(pB)
+# 
+# 
+# pBBorder <- getBorders(spdf = pB, id = "id" )
+# xx <- pBBorder[1,]$geometry[[1]]
+# FU <- pBBorder["FR_UK",]$geometry[[1]][[1]]
+# FUs <- unique(round(FU,0))
+# row.names(FUs) <- 1:nrow(FUs)
+# i <- 33
+# text(FUs[i,1],FUs[i,2], labels = row.names(FUs)[i], col = "red", cex = 2)
+# text
+# 
+# ?runif
+# 
+# 
+# 
+# 
+# 
+# 
+# floor(507/2)
+# 
+# pBBorder["FR_UK",]$geometry[[1]][[1]] <- pBBorder["FR_UK",]$geometry[[1]][[1]][1:253,]
+# 734/2
+# 
+# pBBorderS <- rgeos::gSimplify(spgeom =  methods::as(pBBorder["FR_UK",], "Spatial"), tol = res)
+# plot(pBBorderS, add=T, col = "red")
+# pBBorderS2 <- st_simplify(x=pBBorder, dTolerance = res, preserveTopology = F)
+# 
+# 
+# plot(pBBorder["FR_UK",]$geometry, add=F, lwd = 2)
+# plot(x$geometry, add=T)
+# 
+# plot(pBBorderS2, add=T, col = 1:10, lwd = 3)
+# 
+# plot(pBBorderS2[pBBorderS2$id1=="IT" & pBBorderS2$id2=="ES",]$geometry, lw = 2, add=F)
+# 
+# 
+# 
+# head(pBBorderS2$geometry)
+# 
+# result <- sp::SpatialLinesDataFrame(sl = pBBorderS, data = pBBorder@data)
+# 
+# row.names(result) <- paste0(row.names(result), "_o")
+# 
+# return(result)
+# }
+# 
+
