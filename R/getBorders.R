9fa9106d96398ef151986d042fa8d87f4c7fd7d3
rCarto <timothee.giraud@cnrs.fr>
2018-12-19 14:38:54 +0100

    fix example

--- R/getBorders.R
+++ R/getBorders.R
@@ -33,7 +33,7 @@
 #' plot(st_geometry(mtq), border = NA, col = "grey60")
 #' # Plot borders
 #' plot(st_geometry(mtq.borders), 
-#'      col = sample(x = rainbow(nrow(nuts0.contig))), 
+#'      col = sample(x = rainbow(nrow(mtq.borders))), 
 #'      lwd = 3, add = TRUE)
 #' @seealso \link{discLayer}, \link{getOuterBorders}
 #' @export
