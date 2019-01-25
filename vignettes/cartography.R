dc7c675f818a000651d9cbf8c1670e781a784584
rCarto <timothee.giraud@cnrs.fr>
2019-01-25 15:53:59 +0100

    vignette, more examples

--- vignettes/cartography.R
+++ vignettes/cartography.R
@@ -26,8 +26,14 @@ tilesLayer(x = mtq.osm)
 # plot municipalities (only borders are plotted)
 plot(st_geometry(mtq), col = NA, border = "grey", add=TRUE)
 # plot population
-propSymbolsLayer(x = mtq, var = "POP", inches = 0.4, col = "brown4",
-  legend.pos = "topright",  legend.title.txt = "Total population")
+propSymbolsLayer(
+  x = mtq, 
+  var = "POP", 
+  inches = 0.4, 
+  col = "brown4",
+  legend.pos = "topright",  
+  legend.title.txt = "Total population"
+)
 # layout
 layoutLayer(title = "Population Distribution in Martinique",
             sources = "Sources: Insee and IGN - 2018\n© OpenStreetMap contributors.\nTiles style under CC BY-SA, www.openstreetmap.org/copyright.",
@@ -64,10 +70,9 @@ choroLayer(
 layoutLayer(title = "Population Distribution in Martinique", 
             sources = "Sources: Insee and IGN - 2018",
             author = paste0("cartography ", packageVersion("cartography")), 
-            frame = FALSE, north = FALSE, tabtitle = TRUE) 
+            frame = FALSE, north = FALSE, tabtitle = TRUE, theme= "sand.pal") 
 # north arrow
 north(pos = "topleft")
-
 
 ## ----pentypoMap, fig.height=6, fig.width=5, margin=TRUE------------------
 library(sf)
@@ -95,20 +100,90 @@ typoLayer(
                           "Simple municipality"),
   legend.pos = "topright",
   legend.title.txt = "", 
-  add=TRUE
+  add = TRUE
 )
 #  plot municipalities
 plot(st_geometry(mtq), lwd = 0.5, border = "grey20", add = TRUE, lty = 3)
 # labels for a few  municipalities
 labelLayer(x = mtq[mtq$STATUS != "Simple municipality",], txt = "LIBGEO", 
            cex = 0.9, halo = TRUE, r = 0.15)
-# north arrow
-north(pos = "topleft", col = "#EED19A")
 # title, source, author
-layoutLayer(title = "Administrative Status", 
-            sources = "Sources: Insee and IGN - 2018",theme = "sand.pal",
+layoutLayer(title = "Administrative Status",
+            sources = "Sources: Insee and IGN - 2018", 
             author = paste0("cartography ", packageVersion("cartography")), 
-            frame = FALSE, north = FALSE, tabtitle = TRUE) 
+            north = FALSE, tabtitle = TRUE, postitle = "right", 
+            col = "white", coltitle = "black") 
+# north arrow
+north(pos = "topleft")
+
+## ----propchoro, fig.height=6, fig.width=5, margin=TRUE-------------------
+library(sf)
+library(cartography)
+# path to the geopackage file embedded in cartography
+path_to_file <- system.file("gpkg/mtq.gpkg", package="cartography")
+# import to an sf object
+mtq <- st_read(dsn = path_to_file, quiet = TRUE)
+# Plot the municipalities
+plot(st_geometry(mtq), col="darkseagreen3", border="darkseagreen4",  
+     bg = "lightblue1", lwd = 0.5)
+# Plot symbols with choropleth coloration
+propSymbolsChoroLayer(
+  x = mtq, 
+  var = "POP", 
+  inches = 0.4,
+  border = "grey50",
+  lwd = 1,
+  legend.var.pos = "topright", 
+  legend.var.title.txt = "Population",
+  var2 = "MED",
+  method = "equal", 
+  nclass = 4, 
+  col = carto.pal(pal1 = "sand.pal", n1 = 4),
+  legend.var2.values.rnd = -2,
+  legend.var2.pos = "left", 
+  legend.var2.title.txt = "Median\nIncome\n(in euros)"
+) 
+# layout
+layoutLayer(title="Population & Wealth in Martinique, 2015", 
+            author = "cartography 2.1.3", 
+            sources = "Sources: Insee and IGN - 2018", 
+            scale = 5, tabtitle = TRUE, frame = FALSE)
+# north arrow
+north(pos = "topleft")
+
+## ----proptypo, fig.height=6, fig.width=5, margin=TRUE--------------------
+library(sf)
+library(cartography)
+# path to the geopackage file embedded in cartography
+path_to_file <- system.file("gpkg/mtq.gpkg", package="cartography")
+# import to an sf object
+mtq <- st_read(dsn = path_to_file, quiet = TRUE)
+# Plot the municipalities
+plot(st_geometry(mtq), col="darkseagreen3", border="darkseagreen4",  
+     bg = "lightblue1", lwd = 0.5)
+# Plot symbols with choropleth coloration
+propSymbolsTypoLayer(
+  x = mtq, 
+  var = "POP", 
+  inches = 0.4,
+  border = "white",
+  lwd = .5,
+  legend.var.pos = "topright", 
+  legend.var.title.txt = "Population",
+  var2 = "STATUS",
+  legend.var2.values.order = c("Prefecture", "Sub-prefecture", 
+                               "Simple municipality"),
+  col = carto.pal(pal1 = "pastel.pal", n1 = 3),
+  legend.var2.pos = c(693000, 1607000), 
+  legend.var2.title.txt = "Administrative\nStatus"
+) 
+# layout
+layoutLayer(title="Population Distribution in Martinique", 
+            author = "cartography 2.1.3", 
+            sources = "Sources: Insee and IGN - 2018", 
+            scale = 5, tabtitle = TRUE, frame = FALSE)
+# north arrow
+north(pos = "topleft")
 
 ## ----labelMap, fig.height=6, fig.width=5, margin=TRUE--------------------
 library(sf)
@@ -140,8 +215,46 @@ layoutLayer(
   author = paste0("cartography ", packageVersion("cartography")), 
   frame = FALSE,
   north = TRUE, 
-  tabtitle = TRUE
+  tabtitle = TRUE, 
+  theme = "green.pal"
 ) 
+
+## ----linkMap, fig.height=6, fig.width=5, margin=TRUE, eval=FALSE---------
+#  library(sf)
+#  library(cartography)
+#  # path to the geopackage file embedded in cartography
+#  path_to_file <- system.file("gpkg/mtq.gpkg", package="cartography")
+#  # import to an sf object
+#  mtq <- st_read(dsn = path_to_file, quiet = TRUE)
+#  mob <- read.csv(system.file("csv/mob.csv", package="cartography"))
+#  mob <- mob[mob$sj != "Simple municipality",]
+#  mtq_mob <- getLinkLayer(
+#    x = mtq,
+#    xid = "INSEE_COM",
+#    df = mob,
+#    dfid = c("i","j")
+#  )
+#  
+#  par(bg="grey25")
+#  # plot municipalities
+#  plot(st_geometry(mtq), col = "grey13", border = "grey25",
+#       bg = "grey25", lwd = 0.5)
+#  gradLinkTypoLayer(x = mtq_mob, df = mob,dfid = c('i','j'),
+#                    var = "fij", var2 = "sj",
+#                    legend.var.frame = F, legend.var2.frame = F,
+#                    legend.var2.title.txt = "Destination",
+#                    legend.var.title.txt = "Nb. of\nCommuters",
+#                    legend.var2.pos = "topright",legend.var.pos = "left",
+#                    col = c("grey85", "red4"),
+#                    breaks = c( 100,  500, 1200, 2500, 4679.0),
+#                    lwd = c(1,4,8,16))
+#  # map layout
+#  layoutLayer(
+#    title = "Commuting to Prefectures in Martinique",
+#    sources = "Sources: Insee and IGN - 2018",
+#    author = paste0("cartography ", packageVersion("cartography")),
+#    frame = FALSE, col = "grey25", coltitle = "white",
+#    tabtitle = TRUE)
 
 ## ----isopleth, fig.height=6, fig.width=5, margin=TRUE--------------------
 library(sf)
@@ -156,15 +269,18 @@ plot(st_geometry(mtq), col = NA, border = NA, bg = "lightblue1")
 smoothLayer(
   x = mtq, 
   var = 'POP',
+  typefct = "exponential",
   span = 4000,
-  nclass = 12,
   beta = 2,
-  mask = mtq, 
-  border = "grey",
-  lwd = 0.1, legend.values.rnd = -3,
+  nclass = 12,
   col = carto.pal(pal1 = 'brown.pal', n1 = 12),
+  border = "grey",
+  lwd = 0.1, 
+  mask = mtq, 
+  legend.values.rnd = -3,
   legend.title.txt = "Population\nPotential",
-  legend.pos = "topright", add=TRUE
+  legend.pos = "topright", 
+  add=TRUE
 )
 # annotation on the map
 text(x = 692582, y = 1611478, cex = 0.8, adj = 0, font = 3,  labels = 
@@ -173,10 +289,9 @@ text(x = 692582, y = 1611478, cex = 0.8, adj = 0, font = 3,  labels =
 layoutLayer(title = "Population Distribution in Martinique",
             sources = "Sources: Insee and IGN - 2018",
             author = paste0("cartography ", packageVersion("cartography")),
-            frame = FALSE, north = FALSE, tabtitle = TRUE)
+            frame = FALSE, north = FALSE, tabtitle = TRUE, theme = "brown.pal")
 # north arrow
 north(pos = "topleft")
-
 
 ## ----grid, fig.height=6, fig.width=5, margin=TRUE------------------------
 library(sf)
@@ -185,7 +300,6 @@ library(cartography)
 path_to_file <- system.file("gpkg/mtq.gpkg", package="cartography")
 # import to an sf object
 mtq <- st_read(dsn = path_to_file, quiet = TRUE)
-
 # Create a grid layer
 mygrid <- getGridLayer(
   x = mtq, 
@@ -205,7 +319,10 @@ choroLayer(x = mygrid, var = "POPDENS", method = "geom", nclass=5,
 layoutLayer(title = "Population Distribution in Martinique", 
             sources = "Sources: Insee and IGN - 2018",
             author = paste0("cartography ", packageVersion("cartography")), 
-            frame = FALSE, north = FALSE, tabtitle = TRUE) 
+            frame = FALSE, north = FALSE, tabtitle = TRUE,
+            theme = "turquoise.pal")
+# north arrow
+north(pos = "topleft")
 
 ## ----discc, fig.height=5, fig.width=5, margin=TRUE-----------------------
 library(sf)
@@ -218,16 +335,15 @@ mtq <- st_read(dsn = path_to_file, quiet = TRUE)
 mtq$POPDENS <- as.numeric(1e6 * mtq$POP / st_area(mtq))
 # Get a SpatialLinesDataFrame of countries borders
 mtq.contig <- getBorders(mtq)
-
+# plot municipalities (only the backgroung color is plotted)
 plot(st_geometry(mtq), col = NA, border = NA, bg = "lightblue1", 
      xlim = c(690574, 745940))
-# Plot the population density
+# Plot the population density with custom breaks
 choroLayer(x = mtq, var = "MED",
            breaks = c(min(mtq$MED), seq(13000, 21000, 2000), max(mtq$MED)),
            col = carto.pal("green.pal", 6),border = "white", lwd = 0.5, 
            legend.pos = "topright", legend.title.txt = "Median Income\n(euros)",
            add = TRUE)
-
 # Plot discontinuities
 discLayer(
   x = mtq.contig, 
@@ -245,26 +361,25 @@ discLayer(
   legend.pos = "right",
   add = TRUE
 )
-
 # Layout
 layoutLayer(title = "Wealth Disparities in Martinique, 2015", 
             author =  paste0("cartography ", packageVersion("cartography")),
             sources = "Sources: Insee and IGN - 2018",
             frame = FALSE, scale = 5, tabtitle = TRUE,theme = "grey.pal")
-
+# north arrow
+north(pos = "topleft")
 
 ## ----sp, fig.height=5, fig.width=7, margin=TRUE--------------------------
 library(sp)
 library(cartography)
 data("nuts2006")
-
 # Plot a layer with the extent of the EU28 countries with only a background color
 plot(nuts0.spdf, border = NA, col = NA, bg = "#A6CAE0")
 # Plot non european space
 plot(world.spdf, col  = "#E3DEBF", border = NA, add = TRUE)
 # Plot Nuts2 regions
 plot(nuts0.spdf, col = "grey60",border = "white", lwd = 0.4, add = TRUE)
-
+# plot the countries population
 propSymbolsLayer(
   spdf = nuts0.spdf, 
   df = nuts0.df, 
@@ -276,11 +391,11 @@ propSymbolsLayer(
   border = "white", 
   legend.title.txt = "Population" 
 )
-
+# layout
 layoutLayer(title = "Population in Europe, 2008",
             sources = "Data: Eurostat, 2008",
             author =  paste0("cartography ", packageVersion("cartography")),
             scale = 500, frame = TRUE, col = "#688994") 
-
+# north arrow
 north("topleft")
 
