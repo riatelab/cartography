a294b0be147d9dc1d5336bc915b432e7dba82c9d
rCarto <timothee.giraud@cnrs.fr>
2019-01-29 14:07:14 +0100

    change default value for automatic size scale bar (NULL=>missing), add "bottomright" and "bottomleft" positions.

--- R/barscale.R
+++ R/barscale.R
@@ -1,12 +1,12 @@
 #' @title Scale Bar
 #' @description Plot a scale bar. 
 #' @name barscale
-#' @param size size of the scale bar in kilometers. If set to NULL, an 
+#' @param size size of the scale bar in kilometers. If size is not set, an 
 #' automatic size is used (1/10 of the map width).
 #' @param lwd width of the scale bar.
 #' @param cex cex of the text.
-#' @param pos position of the legend, default to the bottom right corner of the map. 
-#' A vector of two coordinates (c(x, y)) is possible.
+#' @param pos position of the legend, default to "bottomright". 
+#' "bottomright" or a vector of two coordinates (c(x, y)) are possible.
 #' @param style style of the legend, either "pretty" or "oldschool". The 
 #' "oldschool" style only uses the "size" parameter.   
 #' @note This scale bar is not accurate on unprojected (long/lat) maps.
@@ -18,8 +18,7 @@
 #' plot(st_geometry(mtq), col = "grey60", border = "grey20")
 #' barscale(size = 5)
 #' barscale(size = 5, lwd = 2, cex = .9, pos = c(714000, 1596000))
-barscale <- function(size = NULL, lwd = 1.5, cex = 0.6, pos = NULL, 
-                     style="pretty"){
+barscale <- function(size, lwd = 1.5, cex = 0.6, pos = "bottomright", style="pretty"){
   # size = 10
   mapExtent <- par()$usr
   x <- mapExtent[1:2]
@@ -27,7 +26,7 @@ barscale <- function(size = NULL, lwd = 1.5, cex = 0.6, pos = NULL,
   inset <- min(diff(x), diff(y)) / 40
   
   # default scale
-  if(is.null(size)){
+  if(missing(size)){
     size <- diff(x)/10
     size <- signif(size, digits = 0)
   }else{
@@ -39,10 +38,18 @@ barscale <- function(size = NULL, lwd = 1.5, cex = 0.6, pos = NULL,
   labelscale <- paste(size / 1000, "km", sep =" ")
   
   # xy pos
-  xscale <- x[2] - inset * 0.5 - size
-  yscale <- y[1] + inset
+  if(pos=="bottomright"){
+    xscale <- x[2] - inset * 0.5 - size
+    yscale <- y[1] + inset
+  }
+  if(pos == "bottomleft"){
+    xscale <- x[1] + inset * 0.5 
+    yscale <- y[1] + inset
+  }
   
-  if(!is.null(pos)){
+  
+  
+  if(!missing(pos)){
     if(is.numeric(pos) & length(pos)==2){
       xscale <- pos[1]
       yscale <- pos[2]
