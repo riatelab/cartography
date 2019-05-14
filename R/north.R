e7edf2ee852b1ba54a8f06569c3f90570f8170f7
tgiraud <timothee.giraud@cnrs.fr>
2019-05-14 16:39:37 +0200

    north aware north arrow

--- R/north.R
+++ R/north.R
@@ -6,6 +6,7 @@
 #' vector of two coordinates in map units (c(x, y)).
 #' @param south plot a south arrow instead.
 #' @param col arrow color.
+#' @param x sf or sp object used to correct the north azimuth
 #' @export
 #' @seealso \link{layoutLayer}
 #' @examples
@@ -17,49 +18,49 @@
 #'                "bottom", "bottomleft", "left", c(746368, 1632993))){
 #'   north(i, south = TRUE)
 #' }
-north <- function(pos = "topright", col = "grey20", south = FALSE){
+north <- function(pos = "topright", col = "grey20", south = FALSE, x = NULL){
   azim <- "N"
+  theta = 0
   mapExtent <- par()$usr
-  x <- mapExtent[1:2]
-  y <- mapExtent[3:4]
-  inset <- min(diff(x), diff(y)) / 40
-  
+  xe <- mapExtent[1:2]
+  ye <- mapExtent[3:4]
+  inset <- min(diff(xe), diff(ye)) / 40
   if (is.numeric(pos) & length(pos)==2){
     xarrow <- pos[1]
     yarrow <- pos[2]
   }else{
     switch(pos, 
            topleft={
-             xarrow <- x[1] + inset * .5
-             yarrow <- y[2] - inset * 2
+             xarrow <- xe[1] + inset * .5
+             yarrow <- ye[2] - inset * 2
            }, 
            bottomright={
-             xarrow <- x[2] - inset * 1.5
-             yarrow <- y[1] + inset * 2
+             xarrow <- xe[2] - inset * 1.5
+             yarrow <- ye[1] + inset * 2
            },  
            topright={
-             xarrow <- x[2] - inset * 1.5
-             yarrow <- y[2] - inset * 2
+             xarrow <- xe[2] - inset * 1.5
+             yarrow <- ye[2] - inset * 2
            },
            bottomleft={
-             xarrow <- x[1] + inset * .5
-             yarrow <- y[1] + inset * 2
+             xarrow <- xe[1] + inset * .5
+             yarrow <- ye[1] + inset * 2
            },
            top={
-             xarrow <- x[1] + diff(x) / 2 -  inset * .5
-             yarrow <- y[2] - inset * 2
+             xarrow <- xe[1] + diff(xe) / 2 -  inset * .5
+             yarrow <- ye[2] - inset * 2
            },
            bottom={
-             xarrow <- x[1] + diff(x) / 2 - inset * .5
-             yarrow <- y[1] + inset * 2
+             xarrow <- xe[1] + diff(xe) / 2 - inset * .5
+             yarrow <- ye[1] + inset * 2
            },
            left={
-             xarrow <- x[1] + inset * .5
-             yarrow <- y[1] + diff(y) * 0.5 - inset 
+             xarrow <- xe[1] + inset * .5
+             yarrow <- ye[1] + diff(ye) * 0.5 - inset 
            },
            right={
-             xarrow <- x[2] - inset * 1.5
-             yarrow <- y[1] + diff(y) * 0.5 - inset 
+             xarrow <- xe[2] - inset * 1.5
+             yarrow <- ye[1] + diff(ye) * 0.5 - inset 
            } 
     )
   }
@@ -72,10 +73,46 @@ north <- function(pos = "topright", col = "grey20", south = FALSE){
     yy <- c(yarrow + inset * 1.5,yarrow, yarrow + inset * 1.5)
     azim <- "S"
   }
+  
+  if(!missing(x)){
+    xcrs <- sf::st_crs(x)
+    xp <- xarrow + inset / 2
+    yp <- yarrow + inset * 1.5
+    A <- sf::st_as_sf(x = data.frame(X=xp, Y=yp), coords = c('X', 'Y'), 
+                  crs = xcrs, remove = FALSE)
+    B <- sf::st_as_sf(x = data.frame(sf::st_coordinates(A) + c(0,-100000)),
+                  coords = c('X', 'Y'), crs = xcrs, remove = FALSE)
+    Ap <- sf::st_transform(A, 4326)
+    Cp <- sf::st_as_sf(x = data.frame(sf::st_coordinates(Ap) + c(0,-2)), 
+                   coords = c('X', 'Y'), crs = 4326, remove=FALSE)
+    C <- sf::st_transform(Cp,xcrs)
+    C[,c("X","Y")] <- sf::st_coordinates(C)
+    DeltaXB = B$X - A$X
+    DeltaYB = B$Y - A$Y
+    DeltaXC = C$X - A$X
+    DeltaYC = C$Y - A$Y
+    VX = c(DeltaXB, DeltaYB)
+    VY = c(DeltaXC, DeltaYC)
+    theta <- acos( sum(VX*VY) / ( sqrt(sum(VX * VX)) * sqrt(sum(VY * VY)) ) )
+    theta <- sign(C$X - B$X) * theta
+    nc <- rot(A,c(xx[1], yy[1]), theta)
+    xx[1] <- nc[1]
+    yy[1] <- nc[2]
+    nc <- rot(A,c(xx[3], yy[3]), theta)
+    xx[3] <- nc[1]
+    yy[3] <- nc[2]
+    
+  }
   polygon(xx, yy, col = col, border = col)
-  text(xarrow + inset * .5, yarrow, azim, adj = c(0.5, 1.5), cex = 0.8,
-       font = 2, col = col)
+  text(xx[1] + (xx[3] - xx[1]) / 2, yy[1], azim, adj = c(0.5, 1.5), cex = 0.8,
+       font = 2, col = col, srt = theta * 180 / pi)
 }
 
+
+rot <- function(A,B, theta){
+  x <- cos(theta) * (B[1] - A$X) - sin(theta) * (B[2] - A$Y) + A$X
+  y <- sin(theta) * (B[1] - A$X) + cos(theta) * (B[2] - A$Y) + A$Y
+  return(c(x,y))
+}
 
 
--- tests/testthat/test_north.R
+++ tests/testthat/test_north.R
@@ -11,4 +11,5 @@ test_that("north arrow plot", {
   plot(st_geometry(mtq))
   expect_silent(fufun())
   expect_silent(north(pos =  c(746368, 1632993), south = TRUE))
+  expect_silent(north(x=mtq))
 })
