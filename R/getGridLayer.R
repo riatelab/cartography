37409564209ec8fac7822aaf394a4544eb7f91d8
tgiraud <timothee.giraud@cnrs.fr>
2019-05-29 13:57:22 +0200

    getGridLayer() only use sf (>= 0.7-3). related to #54, should fix

--- R/getGridLayer.R
+++ R/getGridLayer.R
@@ -32,7 +32,7 @@
 #'            legend.title.txt = "Population density")
 #' par(opar)
 #' @export
-getGridLayer <- function(x, cellsize, type = "regular", var, 
+getGridLayer <- function(x, cellsize, type = "regular", var,
                          spdf, spdfid = NULL){
   # sp check
   if(missing(x)){
@@ -41,44 +41,35 @@ getGridLayer <- function(x, cellsize, type = "regular", var,
   if (methods::is(x, 'Spatial')){
     x <- sf::st_as_sf(x)
   }
-  
   if(sum(missing(spdf), is.null(spdfid)) != 2){
     warning("spdf and spdfid are deprecated; use x instead.", call. = FALSE)
   }
-  
   x$area <- sf::st_area(x)
-  
-  
   # get a grid
   if(type %in% c("regular", "hexagonal")){
-    cellsize <- as.numeric(cellsize)
-    grid <- switch(type, 
-                   regular = getGridSquare(x, cellsize), 
-                   hexagonal = getGridHexa(x, cellsize))
+    grid <- switch(type,
+                   regular = getGridAll(x, cellsize, TRUE),
+                   hexagonal = getGridAll(x, cellsize, FALSE))
   }else{
     stop("type should be either 'regular' or 'hexagonal'", call. = FALSE)
   }
-  
   # keep only intersecting cells
   gover <- sf::st_intersects(grid, x)
-  fun1 <- function(x) {if(length(x)>0){TRUE}else{FALSE}} 
+  fun1 <- function(x) {if(length(x)>0){TRUE}else{FALSE}}
   grid <- grid[unlist(lapply(gover, FUN = fun1)), ]
-  
   # predicted warning, we don't care...
   options(warn = -1)
   parts <- sf::st_intersection(x = grid[,"id_cell"], y = x)
   options(warn = 0)
   parts$area_part <- sf::st_area(parts)
-  
   lvar <- vector(mode = "list", length = length(var))
   names(lvar) <- var
   for (i in 1:length(lvar)){
-    lvar[[i]] <- as.vector(parts[[names(lvar)[i]]] * parts$area_part / 
+    lvar[[i]] <- as.vector(parts[[names(lvar)[i]]] * parts$area_part /
                              parts$area)
   }
-  v <- aggregate(do.call(cbind,lvar), by = list(id = parts[['id_cell']]), 
+  v <- aggregate(do.call(cbind,lvar), by = list(id = parts[['id_cell']]),
                  FUN = sum, na.rm=TRUE)
-  
   # grid agg
   # split parts
   l <- split(parts,  parts[[1]])
@@ -86,27 +77,29 @@ getGridLayer <- function(x, cellsize, type = "regular", var,
   a <- lapply(l, FUN = function(x){sf::st_buffer(sf::st_union(x), dist = 0.0000001)})
   # only polygons on   # bind all parts
   geometry <- sf::st_cast(do.call(c, a))
-  # full sf 
+  # full sf
   grid <- sf::st_sf(geometry, id = names(l))
   grid$gridarea <- sf::st_area(x = grid)
   
   grid <- merge(grid, v, by = "id", all.x = TRUE)
   return(grid)
-  
 }
 
-
-getGridSquare <- function(x, cellsize){
+getGridAll <- function(x, cellsize, square){
   # cellsize transform
-  cellsize <- sqrt(cellsize)
+  if (square){
+    cellsize <- sqrt(cellsize)
+  }else{
+    cellsize <- 2 * sqrt(cellsize/((3*sqrt(3)/2))) * sqrt(3)/2
+  }
   boundingBox <- sf::st_bbox(x)
   rounder <- boundingBox[1:2] %% cellsize
   boundingBox[1] <- boundingBox[1] - rounder[1]
   boundingBox[2] <- boundingBox[2] - rounder[2]
-  n <- unname(c(ceiling(diff(boundingBox[c(1, 3)]) / cellsize), 
+  n <- unname(c(ceiling(diff(boundingBox[c(1, 3)]) / cellsize),
                 ceiling(diff(boundingBox[c(2, 4)]) / cellsize)))
-  grid <- sf::st_make_grid(cellsize = cellsize, offset = boundingBox[1:2], n = n,
-                           crs = sf::st_crs(x))
+  grid <- sf::st_make_grid(x = x, cellsize = cellsize, offset = boundingBox[1:2],
+                           n = n, crs = sf::st_crs(x), square = square)
   grid <- sf::st_sf(id_cell=1:length(grid), geometry = grid)
   # grid$id_cell <- 1:nrow(grid)
   row.names(grid) <- grid$id_cell
@@ -114,41 +107,7 @@ getGridSquare <- function(x, cellsize){
 }
 
 
-getGridHexa <- function(x, cellsize){
-  
-  #  •*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•
-  #
-  #  This one is still in sp style !
-  #
-  #  •*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•*´¨`*•.¸¸.•
-  
-  spdf <- methods::as(x, "Spatial")
-  # cellsize transform
-  cellsize <- 2 * sqrt(cellsize/((3*sqrt(3)/2))) * sqrt(3)/2
-  bbox <- sp::bbox(spdf)
-  bbox[, 1] <- bbox[, 1] - cellsize
-  bbox[, 2] <- bbox[, 2] + cellsize
-  bboxMat <- rbind( c(bbox[1,'min'] , bbox[2,'min']), 
-                    c(bbox[1,'min'],bbox[2,'max']),
-                    c(bbox[1,'max'],bbox[2,'max']), 
-                    c(bbox[1,'max'],bbox[2,'min']), 
-                    c(bbox[1,'min'],bbox[2,'min']) ) 
-  bboxSP <- sp::SpatialPolygons(Srl = 
-                                  list(sp::Polygons(list(sp::Polygon(bboxMat)),
-                                                    "bbox")), 
-                                proj4string=sp::CRS(sp::proj4string(spdf)))
-  
-  pregrid <- sp::spsample(x = bboxSP, type = "hexagonal", cellsize = cellsize, 
-                          bb = sp::bbox(spdf))
-  grid <- sp::HexPoints2SpatialPolygons(pregrid)
-  grid <- sp::SpatialPolygonsDataFrame(Sr = grid, 
-                                       data = data.frame(id_cell = 
-                                                           1:length(grid)), 
-                                       match.ID = FALSE)
-  row.names(grid) <- as.character(grid$id_cell)
-  grid <- sf::st_as_sf(grid)
-  return(grid)
-}
+
 
 
 #' @title Compute Data for a Grid Layer
@@ -162,83 +121,3 @@ getGridHexa <- function(x, cellsize){
 getGridData <- function(x, df, dfid = NULL, var){
   .Defunct(msg = "This function is defunct, use getGridLayer instead.")
 }
-
-
-
-
-
-
-### ready for sf 0.8....
-# getGridLayer <- function(x, cellsize, type = "regular", var, 
-#                          spdf, spdfid = NULL){
-#   # sp check
-#   if(missing(x)){
-#     x <- sf::st_as_sf(spdf)
-#   }
-#   if (methods::is(x, 'Spatial')){
-#     x <- sf::st_as_sf(x)
-#   }
-#   if(sum(missing(spdf), is.null(spdfid)) != 2){
-#     warning("spdf and spdfid are deprecated; use x instead.", call. = FALSE)
-#   }
-#   x$area <- sf::st_area(x)
-#   # get a grid
-#   if(type %in% c("regular", "hexagonal")){
-#     grid <- switch(type,
-#                    regular = getGridAll(x, cellsize, TRUE),
-#                    hexagonal = getGridAll(x, cellsize, FALSE))
-#   }else{
-#     stop("type should be either 'regular' or 'hexagonal'", call. = FALSE)
-#   }
-#   # keep only intersecting cells
-#   gover <- sf::st_intersects(grid, x)
-#   fun1 <- function(x) {if(length(x)>0){TRUE}else{FALSE}} 
-#   grid <- grid[unlist(lapply(gover, FUN = fun1)), ]
-#   # predicted warning, we don't care...
-#   options(warn = -1)
-#   parts <- sf::st_intersection(x = grid[,"id_cell"], y = x)
-#   options(warn = 0)
-#   parts$area_part <- sf::st_area(parts)
-#   lvar <- vector(mode = "list", length = length(var))
-#   names(lvar) <- var
-#   for (i in 1:length(lvar)){
-#     lvar[[i]] <- as.vector(parts[[names(lvar)[i]]] * parts$area_part / 
-#                              parts$area)
-#   }
-#   v <- aggregate(do.call(cbind,lvar), by = list(id = parts[['id_cell']]), 
-#                  FUN = sum, na.rm=TRUE)
-#     # grid agg
-#   # split parts
-#   l <- split(parts,  parts[[1]])
-#   # aggregate each parts
-#   a <- lapply(l, FUN = function(x){sf::st_buffer(sf::st_union(x), dist = 0.0000001)})
-#   # only polygons on   # bind all parts
-#   geometry <- sf::st_cast(do.call(c, a))
-#   # full sf 
-#   grid <- sf::st_sf(geometry, id = names(l))
-#   grid$gridarea <- sf::st_area(x = grid)
-#   
-#   grid <- merge(grid, v, by = "id", all.x = T)
-#   return(grid)
-# }
-# 
-# getGridAll <- function(x, cellsize, square){
-#   # cellsize transform
-#   if (square){
-#     cellsize <- sqrt(cellsize)
-#   }else{
-#     cellsize <- 2 * sqrt(cellsize/((3*sqrt(3)/2))) * sqrt(3)/2
-#   }
-#   boundingBox <- sf::st_bbox(x)
-#   rounder <- boundingBox[1:2] %% cellsize
-#   boundingBox[1] <- boundingBox[1] - rounder[1]
-#   boundingBox[2] <- boundingBox[2] - rounder[2]
-#   n <- unname(c(ceiling(diff(boundingBox[c(1, 3)]) / cellsize), 
-#                 ceiling(diff(boundingBox[c(2, 4)]) / cellsize)))
-#   grid <- sf::st_make_grid(x = x, cellsize = cellsize, offset = boundingBox[1:2], 
-#                            n = n, crs = sf::st_crs(x), square = square)
-#   grid <- sf::st_sf(id_cell=1:length(grid), geometry = grid)
-#   # grid$id_cell <- 1:nrow(grid)
-#   row.names(grid) <- grid$id_cell
-#   return(grid)
-# }
