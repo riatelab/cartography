0d66ea36b3608610fc769aaac3d2bb409efe37f8
rCarto <timothee.giraud@cnrs.fr>
2019-11-13 16:25:57 +0100

    tests

--- inst/tinytest/test_getTiles.R
+++ inst/tinytest/test_getTiles.R
@@ -3,11 +3,11 @@ library(sp)
 mtq <- st_read(system.file("gpkg/mtq.gpkg", package="cartography"), quiet = TRUE)
 home <- length(unclass(packageVersion("cartography"))[[1]]) == 4
 if(home){
-  expect_true(methods::is(getTiles(x = mtq), "RasterBrick"))
-  expect_message(getTiles(x = mtq, verbose=TRUE))
+  expect_true(methods::is(getTiles(x=mtq, crop = TRUE, verbose = TRUE), "RasterBrick"))
+  expect_warning(getTiles(x = mtq, verbose=TRUE))
   expect_warning(getTiles(spdf = as(mtq, "Spatial"), zoom = 1))
   expect_true(methods::is(getTiles(x = as(mtq, "Spatial")), "RasterBrick"))
-  x <- getTiles(x=mtq)
+  x <- getTiles(x = mtq)
   st_crs(mtq) <- NA
   expect_error(getTiles(x = mtq, zoom = 1))
   expect_silent(tilesLayer(x, add = FALSE))
