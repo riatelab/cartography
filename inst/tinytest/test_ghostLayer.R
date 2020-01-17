d03269354e01426b5ecf138191968d098968c551
rCarto <timothee.giraud@cnrs.fr>
2020-01-17 16:49:01 +0100

    correct test for ghostLayer()

--- inst/tinytest/test_ghostLayer.R
+++ inst/tinytest/test_ghostLayer.R
@@ -2,5 +2,5 @@ library(sf)
 library(sp)
 mtq <- st_read(system.file("gpkg/mtq.gpkg", package="cartography"), quiet=TRUE)
 target <- mtq[30,]
-expect_silent(shadowLayer(target, bg = "lightblue"))
-expect_silent(shadowLayer(as(target, "Spatial")))
+expect_silent(ghostLayer(target, bg = "lightblue"))
+expect_silent(ghostLayer(as(target, "Spatial")))
