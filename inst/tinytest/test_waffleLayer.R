c07fded6f09eba39ef7e1084c5d0b3cc00ed52c9
rCarto <timohee.giraud@cnrs.fr>
2020-04-02 13:41:47 +0200

    tests

--- inst/tinytest/test_waffleLayer.R
+++ inst/tinytest/test_waffleLayer.R
@@ -2,5 +2,7 @@ library(sf)
 library(sp)
 mtq <- st_read(system.file("gpkg/mtq.gpkg", package="cartography"), quiet = TRUE)
 mtq$EMP <- mtq$ACT - mtq$CHOM
-expect_silent(waffleLayer(x = mtq, var = c("CHOM", "ACT"), cellvalue = 5000))
+expect_silent(waffleLayer(x = mtq, var = c("CHOM", "ACT"), cellvalue = 5000, add = FALSE))
+plot(st_geometry(mtq))
+expect_silent(waffleLayer(x = mtq, var = c("CHOM", "ACT"), cellvalue = 5000, add = TRUE))
 expect_error(waffleLayer(x = mtq, var = c("CHOM", "ACT"), cellvalue = 5000, cellrnd = "rndd"))
