bfe261a3e1d8feab1ca3c467890cb8dd37295184
rCarto <timothee.giraud@cnrs.fr>
2021-03-18 13:12:27 +0100

    correct startup msg

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,6 @@
 .onAttach <- function(libname, pkgname) {
   msg <- paste0(
-    "This project enters in maintenance mode. 
+    "This project is in maintenance mode. 
 Core functionalities of `cartography` can be found in `mapsf`.
 https://riatelab.github.io/mapsf/"
   )
