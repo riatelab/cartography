7fb796401ec633273233a57fbb69d86792da4b6b
rCarto <timothee.giraud@cnrs.fr>
2021-03-18 12:49:56 +0100

    add a startup msg

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,8 @@
 .onAttach <- function(libname, pkgname) {
-  msg <- ""
-  packageStartupMessage("Data: (c) OpenStreetMap contributors, ODbL 1.0 - http://www.openstreetmap.org/copyright")
-  packageStartupMessage("Routing: OSRM - http://project-osrm.org/")
-  # options(osrm.server = "http://0.0.0.0:5000/", osrm.profile = "driving")
+  msg <- paste0(
+    "This project enters in maintenance mode. 
+Core functionalities of `cartography` can be found in `mapsf`.
+https://riatelab.github.io/mapsf/"
+  )
+  packageStartupMessage(msg)
 }
